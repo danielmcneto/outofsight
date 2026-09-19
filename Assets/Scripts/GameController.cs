d85d6291316594b51496fa158235d7e4b9a423a4
user123 <mylovelycar.2010@gmail.com>
2026-09-19 18:57:00 +0400

    Implemented door, key system and setup shoulder peeking

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,12 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public List<ItemEntry> items = new List<ItemEntry>();
+    public PlayerBrain player;
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<PlayerBrain>();
+    }
 }
 
 public enum ItemType
